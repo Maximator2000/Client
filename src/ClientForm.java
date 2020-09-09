07fd97aa4fb5f0d803e3309b3f553bbaebe48505
gordmaxi <gloriavibo1@gmail.com>
2020-09-09 13:16:33 +0200

    fenster

---
+++ src/ClientForm.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class ClientForm {
+    private JPanel panel;
+    private JTextField clientTextField;
+    private JTextField nameField;
+    private JTextField remotalIPField2;
+    private JTextField portTextField;
+    private JTextField textField1;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton connectButton;
+    private JTextField textField2;
+    private JButton sendButton;
+    private JTextField receivedTextField;
+    private JScrollBar scrollBar1;
+}
